1d319ca5ae63d11b4b8e392278b1c1188a5b596e
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-07-28 07:22:52 +0300

    Remove StdLibArraysTest and include collections/ArraysTest.kt into StdLibTestToJSTest smoke tests instead. Move helper methods, so they are available in JS semantics tests.

--- libraries/stdlib/test/collections/CollectionTest.kt
+++ libraries/stdlib/test/collections/CollectionTest.kt
@@ -654,18 +654,3 @@ class CollectionTest {
     }
 
 }
-
-fun <T> Iterable<T>.assertSorted(isInOrder: (T, T) -> Boolean): Unit { this.iterator().assertSorted(isInOrder) }
-fun <T> Iterator<T>.assertSorted(isInOrder: (T, T) -> Boolean) {
-    if (!hasNext()) return
-    var index = 0
-    var prev = next()
-    while (hasNext()) {
-        index += 1
-        val next = next()
-        assertTrue(isInOrder(prev, next), "Not in order at position $index, element[${index-1}]: $prev, element[$index]: $next")
-        prev = next
-    }
-    return
-}
-
